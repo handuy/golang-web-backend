38ae1aa7f8d4d7534775bd985e71e04e3de07ce8
duy <duy@techmaster.vn>
2019-04-03 15:50:53 +0700

    cập nhật ví dụ subdomain

---
+++ subdomain/Readme.md
@@ -0,0 +1,26 @@
+Cài đặt kataras iris:
+```go
+go get -u github.com/kataras/iris
+```
+
+Cập nhật file hosts tại: /etc/hosts. Thêm các dòng sau:
+```
+127.0.0.1       techmaster.local
+127.0.0.1       golang.techmaster.local
+127.0.0.1       java.techmaster.local
+
+```
+
+Chạy code:
+```go
+go run main.go
+```
+
+Ví dụ gồm:
+- 1 Domain chính: techmaster.local:8080
+- 2 subdomain: golang.techmaster.local:8080 và java.techmaster.local:8080. Mỗi subdomain có 3 đường dẫn: /, /course và /secret, hiển thị nội dung khác nhau
+
+Khi truy cập vào /secret ở một subdomain, ví dụ golang.techmaster.local:8080/secret:
+- Nếu user chưa đăng nhập: Hệ thống trả về HTTP status: Forbidden. Truy cập vào java.techmaster.local:8080/secret cũng bị forbidden
+- User truy cập vào golang.techmaster.local:8080/login:  Hệ thống sẽ redirect về trang login của domain chính: techmaster.local:8080/login để user đăng nhập. Sau khi đăng nhập, hệ thống sẽ redirect lại về trang golang.techmaster.local:8080/secret. Lúc này server trả về HTTP 200 OK
+- User đã đăng nhập golang.techmaster.local:8080 thì cũng sẽ đăng nhập ở java.techmaster.local:8080. Truy cập vào java.techmaster.local:8080/secret cũng trả về HTTP 200 OK
--- subdomain/main.go
+++ subdomain/main.go
@@ -6,68 +6,96 @@ import (
 )
 
 var (
-    cookieNameForSessionID = "mycookiesessionnameid"
-    sess                   = sessions.New(sessions.Config{Cookie: cookieNameForSessionID, AllowReclaim: true})
+	cookieNameForSessionID = "mycookiesessionnameid"
+	sess                   = sessions.New(sessions.Config{Cookie: cookieNameForSessionID, AllowReclaim: true})
+	redirectAfterLogin     = ""
+	mainDomain             = "techmaster.local:8080"
 )
 
 func secret(ctx iris.Context) {
-    // Check if user is authenticated
-    if auth, _ := sess.Start(ctx).GetBoolean("authenticated"); !auth {
-        ctx.StatusCode(iris.StatusForbidden)
-        return
-    }
+	// Check if user is authenticated
+	if auth, _ := sess.Start(ctx).GetBoolean("authenticated"); !auth {
+		ctx.StatusCode(iris.StatusForbidden)
+		return
+	}
 
 	// Print secret message
 	subdomain := ctx.Subdomain()
-    ctx.Writef("Hello %s", subdomain)
+	ctx.Writef("Đây là tin nhắn từ subdomain %s", subdomain)
 }
 
 func login(ctx iris.Context) {
-    session := sess.Start(ctx)
+	session := sess.Start(ctx)
 
-    // Authentication goes here
-    // ...
+	// Authentication goes here
+	// ...
 
-    // Set user as authenticated
+	// Set user as authenticated
 	session.Set("authenticated", true)
-	ctx.WriteString("Login thanh cong")
+	ctx.Redirect(redirectAfterLogin)
 }
 
-func loginRedirect(ctx iris.Context) {
-	ctx.Redirect("http://domain.local:8080/login")
+func logout(ctx iris.Context) {
+	session := sess.Start(ctx)
+
+	// Revoke users authentication
+	session.Set("authenticated", false)
+
+	subdomain := ctx.Subdomain()
+	ctx.Writef("Logout từ subdomain %s", subdomain)
+}
+
+// redirect về domain chính http://domain.local:8080/login để xử lý logic đăng nhập
+func dashboardLoginRedirect(ctx iris.Context) {
+	redirectAfterLogin = "http://golang.techmaster.local:8080/secret"
+	ctx.Redirect("http://" + mainDomain + "/login")
+}
+
+// redirect về domain chính http://domain.local:8080/login để xử lý logic đăng nhập
+func systemLoginRedirect(ctx iris.Context) {
+	redirectAfterLogin = "http://java.techmaster.local:8080/secret"
+	ctx.Redirect("http://" + mainDomain + "/login")
 }
 
 func main() {
 	app := iris.New()
 
-	/*
-	 * Setup static files
-	 */
-
-	app.StaticWeb("/assets", "./public/assets")
-	app.StaticWeb("/upload_resources", "./public/upload_resources")
-
-	// dashboard.domain.local:8080
-	dashboard := app.Subdomain("dashboard")
+	// golang.techmaster.local:8080
+	dashboard := app.Subdomain("golang")
 	dashboard.Get("/", func(ctx iris.Context) {
-		ctx.Writef("HEY FROM dashboard")
+		subdomain := ctx.Subdomain()
+		ctx.Writef("Hello %s", subdomain)
+	})
+	dashboard.Get("/course", func(ctx iris.Context) {
+		ctx.Writef("Hello golang course")
 	})
 	dashboard.Get("/secret", secret)
-	dashboard.Get("/login", loginRedirect)
+	dashboard.Get("/login", dashboardLoginRedirect)
+	dashboard.Get("/logout", logout)
 
-	// system.domain.local:8080
-	system := app.Subdomain("system")
+	// java.techmaster.local:8080
+	system := app.Subdomain("java")
 	system.Get("/", func(ctx iris.Context) {
-		ctx.Writef("HEY FROM system")
+		subdomain := ctx.Subdomain()
+		ctx.Writef("Hello %s", subdomain)
+	})
+	system.Get("/course", func(ctx iris.Context) {
+		ctx.Writef("Hello java course")
 	})
 	system.Get("/secret", secret)
-	system.Get("/login", loginRedirect)
+	system.Get("/login", systemLoginRedirect)
+	system.Get("/logout", logout)
 
-	// domain.local:8080
+	// techmaster.local:8080
 	app.Get("/", func(ctx iris.Context) {
-		ctx.Writef("HEY FROM frontend /")
+		ctx.Writef("Hello Techmaster")
+	})
+	app.Get("/course", func(ctx iris.Context) {
+		ctx.Writef("Hello techmaster courses")
 	})
 	app.Get("/login", login)
+	app.Get("/logout", logout)
+	app.Get("/secret", secret)
 
-	app.Run(iris.Addr("domain.local:8080")) // for beginners: look ../hosts file
+	app.Run(iris.Addr(mainDomain))
 }
